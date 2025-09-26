4ca9bbffe3fa5d0ba2fb222d63a4fec222abe092
Dmitry Samodurov <dsamodurov@bithusky.io>
2025-09-26 14:10:35 +0300

    Update dependencies and adjust Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,6 @@ export default defineConfig({
             ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
-        tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
         vue({
             template: {
                 transformAssetUrls: {
@@ -22,6 +18,10 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+        tailwindcss(),
+        wayfinder({
+            formVariants: true,
         }),
     ],
 });
